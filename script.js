17fe9e4da058117c974a3644a97869699e55e626
Angelina Minshall <atminshall@nic.edu>
2023-12-08 12:21:20 -0800

    modal help

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const links = document.querySelector('.links');
 navToggle.addEventListener('click', function(){
     links.classList.toggle("show-links");
 });
+// button modal
+
+// Get the modal and buttons
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+
+// Function to open the modal
+function openModal() {
+    modal.style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Event listeners for the buttons
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+  
